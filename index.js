01f80c6b037ad04cf08dea35f9cd18ebbeaa10b3
iskandervulto <iskanderhyvulto@gmail.com>
2023-06-04 12:41:50 -0400

    B

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 // Write your solution here!
+const cats= [
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(name) {
+    cats.push(`${name}`)
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyPrependCat(name) {
+    cats.unshift(`${name}`)
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop (`${name}`)
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(`${name}`)
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function appendCat(name) {
+   return [...cats, `${name}`]
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function prependCat(name) {
+    return [`${name}`, ...cats]
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function removeLastCat() {
+   return cats.slice(0, cats.length - 1)
+  }
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function removeFirstCat() {
+   return cats.slice (1, cats.length)
+  }
